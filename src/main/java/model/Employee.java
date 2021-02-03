6795b4a314639e23901ed7e0f0dbf3c0b52bfedf
SpideyStan581994 <steven.tan@ulaps.dev>
2021-02-03 18:51:42 +0800

    Added the Employee Model.

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,updatable = false)
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+    @Column(nullable = false,updatable = false)
+    private String employeeCode;
+
+
+}
